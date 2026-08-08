936dfd558a4d4c80a2f324fd897f18bac145cf97
m8wfy4md26-cyber <m8wfy4md26@privaterelay.appleid.com>
2026-08-08 08:24:13 +0300

    fix contact route and add logs

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
+    console.log("=== ROUTE STARTED ===");
   try {
     const { name, phone, message } = await req.json();
 
